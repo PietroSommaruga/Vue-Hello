cdc193b988807e53039d7307b14b12d08a4be474
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-01-13 17:57:21 +0100

    fine

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+        message: 'Shrek',
+        shrek: 'https://www.leggilo.org/wp-content/uploads/2021/12/shrek-1.jpg',
+        userName: '',
+    },
+    
+    
+})
